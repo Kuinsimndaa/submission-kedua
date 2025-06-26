a524e56990ee200a80e119e09462609db336cdee
Kuinsi MandaA <queencymanda@gmail.com>
2025-06-26 12:14:52 +0700

    fix: skip to content now works without page refresh in SPA

--- index.js
+++ index.js
@@ -5,4 +5,15 @@ import { initRouter } from './src/router.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   initRouter();
+  // Skip to content fix for SPA
+  const mainContent = document.querySelector('#main-content');
+  const skipLink = document.querySelector('.skip-link');
+  if (mainContent && skipLink) {
+    skipLink.addEventListener('click', function (event) {
+      event.preventDefault();
+      skipLink.blur();
+      mainContent.focus();
+      mainContent.scrollIntoView();
+    });
+  }
 });
